2188c82b02519252357d26670db3e8cf2f54032b
pourezzat <amirrezapourezzat@gmail.com>
2022-07-27 16:27:37 +0430

    initail

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const _dollarPrice = 32000;
 const dollar = document.querySelectorAll('input')[0]
 const rial = document.querySelectorAll('input')[1]
-
 function changeToRial(){
   rial.value = dollar.value*_dollarPrice
 }
